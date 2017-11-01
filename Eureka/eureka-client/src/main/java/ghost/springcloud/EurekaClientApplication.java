467a48ba80830d3c82aa3f0577289b477eb7025e
jack <jack_chow621@sina.com>
2017-11-01 21:12:16 +0800

    上传Eureka模块代码
    上传Eureka模块代码

---
+++ Eureka/eureka-client/src/main/java/ghost/springcloud/EurekaClientApplication.java
@@ -0,0 +1,26 @@
+package ghost.springcloud;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@EnableDiscoveryClient  // 通过该注解，实现服务发现，注册
+@SpringBootApplication
+@RestController
+public class EurekaClientApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaClientApplication.class, args);
+	}
+
+	@Value("${server.port}")
+	String port;
+
+	@RequestMapping("/hi")
+	public String home(@RequestParam String name) {
+		return "hi " + name + ",i am from port:" + port;
+	}
+}
